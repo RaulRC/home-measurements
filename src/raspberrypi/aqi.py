ba245476b7b2e863d5fe4819838118b2ef2976ae
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2023-12-15 17:51:54 +0100

    [ADD] files for aqi

---
+++ src/raspberrypi/aqi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+# Requires pyserial. Install via:
+# pip install pyserial
+
+from __future__ import print_function
+import argparse
+from serial import Serial, EIGHTBITS, STOPBITS_ONE, PARITY_NONE
+import time, struct, datetime, json
+import argparse
+
+parser = argparse.ArgumentParser(description='AQI measurement script')
+parser.add_argument('--path', type=str, default="/home/pi/measurements/", help='Path to save measurement files')
+parser.add_argument('--port', type=str, default="/dev/ttyUSB0", help='Serial port')
+parser.add_argument('--baudrate', type=int, default=9600, help='Baudrate')
+args = parser.parse_args()
+
+PATH = args.path
+port = args.port
+baudrate = args.baudrate
+
+
+def main():
+    # Prepare serial connection.
+    ser = Serial(port, baudrate=baudrate, bytesize=EIGHTBITS, parity=PARITY_NONE, stopbits=STOPBITS_ONE)
+    ser.flushInput()
+
+    HEADER_BYTE = b"\xAA"
+    COMMANDER_BYTE = b"\xC0"
+    TAIL_BYTE = b"\xAB"
+
+    byte, previousbyte = b"\x00", b"\x00"
+
+    while True:
+        previousbyte = byte
+        byte = ser.read(size=1)
+        
+        # We got a valid packet header.
+        if previousbyte == HEADER_BYTE and byte == COMMANDER_BYTE:
+            packet = ser.read(size=8) # Read 8 more bytes
+            
+            # Decode the packet - little endian, 2 shorts for pm2.5 and pm10, 2 ID bytes, checksum.
+            readings = struct.unpack('<HHcccc', packet)
+            
+            # Measurements.
+            pm_25 = readings[0]/10.0
+            pm_10 = readings[1]/10.0
+            
+            # ID
+            id = packet[4:6]
+            
+            # Prepare checksums.
+            checksum = readings[4][0]
+            calculated_checksum = sum(packet[:6]) & 0xFF
+            checksum_verified = (calculated_checksum == checksum)
+            
+            # Message tail.
+            tail = readings[5]
+            
+            if tail == TAIL_BYTE and checksum_verified:
+                timestamp = str(datetime.datetime.now())
+                for item, value in zip(['pm_25', 'pm_10'], [pm_25, pm_10]):
+                    result = dict()
+                    result['timestamp'] = timestamp
+                    result['key'] = item
+                    result['value'] = value
+                    with open(f"{PATH}{timestamp}-{item}.json", 'w') as fp:
+                        json.dump(result, fp)
+                        print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ src/raspberrypi/temp.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+# Requires pyserial. Install via:
+# pip install pyserial
+
+from __future__ import print_function
+from serial import Serial, EIGHTBITS, STOPBITS_ONE, PARITY_NONE
+import sys
+import time
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import datetime
+import requests
+import time, struct
+import sys
+import os
+
+
+
+HOST = sys.argv[1]
+PORT = sys.argv[2]
+AQI_FOLDER = sys.argv[3]
+
+URL = f"http://{HOST}:{PORT}/sensor-data"
+
+def getSensorData():
+   """
+   Get temperature and humidity data from the DHT22 sensor.
+
+   Returns:
+      tuple: A tuple containing the humidity and temperature values.
+   """
+   RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+   return (str(RH), str(T))
+
+def empty_buffer(buff):
+   """
+   Empty the buffer by sending the buffered data to the server.
+
+   Args:
+      buff (list): The buffer containing the data to be sent.
+
+   Returns:
+      list: The updated buffer after sending the data.
+   """
+   while len(buff) > 0:
+      pl = buff.pop()
+      sc = send_request(pl)
+      if sc != 200:
+         buff.append(pl)
+         break
+      else:
+         print("Emptying buffer", sc, pl)
+   return buff
+
+def send_request(pl):
+   """
+   Send a POST request to the server with the payload.
+
+   Args:
+      pl (dict): The payload to be sent.
+
+   Returns:
+      int: The status code of the HTTP response.
+   """
+   r = requests.post(URL, json=pl, timeout=3)
+   return r.status_code
+
+def send_aqi_data():
+   """
+   Send measurements from every file in the specified folder.
+
+   Args:
+      folder_path (str): The path to the folder containing the JSON files.
+
+   Returns:
+      None
+   """
+   for filename in os.listdir(AQI_FOLDER):
+      file_path = os.path.join(AQI_FOLDER, filename)
+      with open(file_path, 'r') as file:
+         try:
+            data = file.read()
+            # Send the data as a request
+            send_request(data)
+            print(f"Sent request for file: {filename}")
+            os.remove(file_path)
+         except Exception as e:
+            print(f"Failed to send request for file: {filename}")
+            print(str(e))
+
+
+def main():
+   """
+   Main function to collect sensor data and send it to the server.
+   """
+   print('Initializing...')
+   buff = list()
+   while True:
+      RH, T = getSensorData()
+      measures = dict()
+      measures['temp'] = str(round(float(T), 2))
+      measures['humidity'] = str(round(float(RH), 2))
+      timestamp = str(datetime.datetime.now())
+
+      for key in ['temp', 'humidity']:
+         try:
+            pl = dict()
+            pl['key'] = key
+            pl['value'] = measures[key]
+            pl['place'] = "whitehouse"
+            pl['room'] = "main floor"
+            pl['timestamp'] = timestamp
+            status_code = send_request(pl)
+            if status_code == 200:
+               print(f"[{status_code}] {timestamp} {pl}")
+               if len(buff) > 0:
+                  buff = empty_buffer(buff)
+               send_aqi_data()
+            else:
+               print("Buffering...", pl, len(buff))
+               buff.append(pl)
+         except Exception as e:
+            print('Terminated.' + str(e))
+            buff.append(pl)
+      time.sleep(60)
+
+if __name__ == '__main__':
+   main()
+
+# To invoke this script, run the following command in the terminal:
+# python temp.py <HOST> <PORT> <AQI_FOLDER>
+# Replace <HOST> with the server host and <PORT> with the server port and <AQI_FOLDER> with the path to the folder containing the AQI JSON files.
